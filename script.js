e7128d256b4dc27248f7916e6742239aee4921da
Sphendami <m.sphendami@gmail.com>
2019-09-01 14:48:24 +0900

    prepared files

---
+++ script.js
@@ -0,0 +1,78 @@
+let display = document.getElementById("display");
+
+let formula = "0";
+let preIsOperator = false;
+let preIsEqual = false;
+
+
+function clickNum(number) {
+    if (preIsEqual) {
+        display.value = number;
+        formula = number;
+    } else if (preIsOperator) {
+        display.value = number;
+        formula += number;
+    } else if (display.value === "0") {
+        display.value = number;
+        if (formula === "0") {
+            formula = number;
+        } else {
+            formula = formula.slice(0, -1) + number;
+        }
+    } else {
+        display.value += number;
+        formula += number;
+    }
+
+    preIsOperator = false;
+    preIsEqual = false;
+
+}
+
+function clickDot() {
+    if (display.value.includes(".")) return;
+
+    display.value += ".";
+    formula += ".";
+
+    preIsOperator = false;
+    preIsEqual = false;
+
+}
+
+function clickOpe(operator) {
+    if (preIsOperator) {
+        formula = formula.slice(0, -1);
+    }
+    let evaluation = eval(formula);
+    formula = evaluation + operator;
+    display.value = evaluation;
+
+    preIsOperator = true;
+    preIsEqual = false;
+
+}
+
+function clickEqual() {
+    if (preIsOperator) {
+        formula = formula.slice(0, -1);
+    }
+    let evaluation = eval(formula);
+    formula = evaluation;
+    display.value = evaluation;
+
+    preIsEqual = true;
+    preIsOperator = false;
+
+}
+
+function clickClear() {
+    display.value = "0";
+    formula = "0";
+
+    preIsOperator = false;
+    preIsEqual = false;
+
+}
+
+
